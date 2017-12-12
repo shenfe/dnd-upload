022f777892bacc49ca59dad0696b1e16bb30db06
hengwu <youchuangqiba@163.com>
2017-12-12 13:12:17 +0800

    upd8

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
-let holder = document.getElementById('holder');
-
-let acceptedTypes = {
+const acceptedTypes = {
     'image/png': true,
+    'image/jpg': true,
     'image/jpeg': true,
     'image/gif': true
 };
 
-function readfile(file) {
+const readfile = function (file, onread) {
     if (acceptedTypes[file.type] === true) {
         let reader = new FileReader();
         reader.onload = function (event) {
             let image = new Image();
             image.src = event.target.result;
-            image.width = 250; // a fake resize
-            holder.appendChild(image);
+            onread && onread(image);
         };
         reader.readAsDataURL(file);
     }
     console.log(file.name + ':' + (file.size ? (file.size / 1024 | 0) + 'K' : ''));
-}
+};
 
-function readfiles(files, progress, readfile) {
+const readfiles = function (files, { apiUrl, data = {}, onprogress, onread }) {
     let formData = new FormData();
     for (let i = 0; i < files.length; i++) {
-        formData.append('file', files[i]);
-        readfile(files[i]);
+        formData.append('file[]', files[i]);
+        readfile(files[i], onread);
     }
-
-    // now post a new XHR request
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/devnull.php');
-    xhr.onload = function () {
-        progress(100);
-    };
-
-    xhr.upload.onprogress = function (event) {
-        if (event.lengthComputable) {
-            let complete = (event.loaded / event.total * 100 | 0);
-            progress(complete);
+    for (let p in data) {
+        if (!data.hasOwnProperty(p)) continue;
+        if (typeof data[p] === 'function') {
+            formData.append(p, data[p]());
+        } else {
+            formData.append(p, data[p]);
         }
     }
 
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', apiUrl);
+    xhr.onload = function () {
+        onprogress(100);
+    };
+    xhr.upload.onprogress = function (event) {
+        if (event.lengthComputable) {
+            let complete = (event.loaded / event.total * 100 | 0);
+            onprogress(complete);
+        }
+    }
     xhr.send(formData);
-}
+};
 
-holder.ondragover = function () { this.className = 'hover'; return false; };
-holder.ondragend = function () { this.className = ''; return false; };
-holder.ondrop = function (e) {
-    this.className = '';
-    e.preventDefault();
-    readfiles(e.dataTransfer.files, () => { }, readfile);
-}
+const bind = (el, options = {}) => {
+    let { apiUrl, data, ondragover, ondragend, ondrop, onprogress, onread } = options;
+
+    el.ondragover = function () {
+        ondragover.apply(this);
+        return false;
+    };
+
+    el.ondragend = function () {
+        ondragend.apply(this);
+        return false;
+    };
+
+    el.ondrop = function (e) {
+        e.preventDefault();
+        ondrop.apply(this);
+        readfiles(e.dataTransfer.files, {
+            apiUrl,
+            data,
+            onprogress,
+            onread
+        });
+    }
+};
+
+module.exports = bind;
